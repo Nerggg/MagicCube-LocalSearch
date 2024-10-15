2e36c8934f2748ff0a96ccb050c0fbc6394edf01
nerggg <mas.ikhwanalhakim@gmail.com>
2024-10-15 10:22:12 +0700

    feat: objective function wip

--- main.go
+++ main.go
@@ -20,6 +20,123 @@ func generateRandomNumbers() []int {
 	return numbers
 }
 
+func calculateObjectiveFunction(cube [5][5][5]int) int {
+	result := 0
+
+	// Iterasi bagian pertama
+	for z := 0; z < 5; z++ {
+		for x := 0; x < 5; x++ {
+			temp := 0
+			for y := 0; y < 5; y++ {
+				temp += cube[x][y][z]
+			}
+			if temp == 315 {
+				result++
+			}
+		}
+	}
+
+	for z := 0; z < 5; z++ {
+		for y := 0; y < 5; y++ {
+			temp := 0
+			for x := 0; x < 5; x++ {
+				temp += cube[x][y][z]
+			}
+			if temp == 315 {
+				result++
+			}
+		}
+	}
+
+	for y := 0; y < 5; y++ {
+		for x := 0; x < 5; x++ {
+			temp := 0
+			for z := 0; z < 5; z++ {
+				temp += cube[x][y][z]
+			}
+			if temp == 315 {
+				result++
+			}
+		}
+	}
+
+	// Iterasi bagian kedua
+	for y := 0; y < 5; y++ {
+		temp := 0
+		for x, z := 0, 0; x < 5 && z < 5; x, z = x+1, z+1 {
+			temp += cube[x][y][z]
+		}
+		if temp == 315 {
+			result++
+		}
+	}
+
+	for y := 0; y < 5; y++ {
+		temp := 0
+		for x, z := 4, 0; x >= 0 && z < 5; x, z = x-1, z+1 {
+			temp += cube[x][y][z]
+		}
+		if temp == 315 {
+			result++
+		}
+	}
+
+	for x := 0; x < 5; x++ {
+		temp := 0
+		for y, z := 0, 0; y < 5 && z < 5; y, z = y+1, z+1 {
+			temp += cube[x][y][z]
+		}
+		if temp == 315 {
+			result++
+		}
+	}
+
+	for x := 0; x < 5; x++ {
+		temp := 0
+		for y, z := 4, 0; y >= 0 && z < 5; y, z = y-1, z+1 {
+			temp += cube[x][y][z]
+		}
+		if temp == 315 {
+			result++
+		}
+	}
+
+	// Iterasi bagian ketiga
+	temp := 0
+	for x, y, z := 0, 0, 0; x < 5 && y < 5 && z < 5; x, y, z = x+1, y+1, z+1 {
+		temp += cube[x][y][z]
+	}
+	if temp == 315 {
+		result++
+	}
+
+	temp = 0
+	for x, y, z := 4, 0, 0; x >= 0 && y < 5 && z < 5; x, y, z = x-1, y+1, z+1 {
+		temp += cube[x][y][z]
+	}
+	if temp == 315 {
+		result++
+	}
+
+	temp = 0
+	for x, y, z := 4, 0, 4; x >= 0 && y < 5 && z >= 0; x, y, z = x-1, y+1, z-1 {
+		temp += cube[x][y][z]
+	}
+	if temp == 315 {
+		result++
+	}
+
+	temp = 0
+	for x, y, z := 0, 0, 4; x < 5 && y < 5 && z >= 0; x, y, z = x+1, y+1, z-1 {
+		temp += cube[x][y][z]
+	}
+	if temp == 315 {
+		result++
+	}
+
+	return result
+}
+
 func main() {
 	var arr [5][5][5]int
 
@@ -45,4 +162,6 @@ func main() {
 		}
 		fmt.Println()
 	}
+
+	fmt.Println("Objective Function Result:", calculateObjectiveFunction(arr))
 }
